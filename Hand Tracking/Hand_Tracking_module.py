bdb625c306edf720b1b1944b7817a7a246bc0e65
trunnguyen <137796467+trunnguyen@users.noreply.github.com>
2025-06-30 12:08:48 +0700

    Update Hand_Tracking_module.py

--- Hand Tracking/Hand_Tracking_module.py
+++ Hand Tracking/Hand_Tracking_module.py
@@ -30,7 +30,8 @@ class handDetector():
         return img
 
     def findPosition(self, img, handNo=0, draw=True):
-        lmlist = []
+        self.lmlist = []
+        bbox=[]
 
         if self.results.multi_hand_landmarks:
 
@@ -41,7 +42,7 @@ class handDetector():
                     h, w, c = img.shape
                     cx, cy = int(lm.x * w), int(lm.y * h)
 
-                    lmlist.append([id, cx, cy])
+                    self.lmlist.append([id, cx, cy])
                     if draw:
 
                         cv2.circle(img, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
@@ -49,7 +50,38 @@ class handDetector():
 
                 pass
 
-        return lmlist
+        return self.lmlist,bbox
+    
+    
+    def fingersUp(self):
+        fingers = []
+
+        # Thumb
+        if self.lmlist[self.tipIds[0]][1] < self.lmlist[self.tipIds[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+        # Fingers
+        for id in range(1, 5):
+            if self.lmlist[self.tipIds[id]][2] < self.lmlist[self.tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        return fingers
+
+    
+    def findDistance(self,p1,p2, img, draw=True,r=15,t=3):
+        x1, y1 = self.lmlist[p1][1:]
+        x2, y2 = self.lmlist[p2][1:]
+        cx,cy=(x1+x2)//2,(y1+y2)//2
+        if draw:
+            cv2.line(img, (x1, y1),(x2, y2),(255,0,255),t)
+            cv2.circle(img, (x1, y1), r, (255,0,255),cv2.FILLED)
+            cv2.circle(img, (x2, y2), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (cx, cy), r, (0, 0, 255), cv2.FILLED)
+        length = math.hypot(x2-x1, y2-y1)
+        return length,img,[x1,y1,x2,y2,x1,y1,cx,cy]
+
 
 
 def main():
@@ -61,7 +93,6 @@ def main():
         return
 
     detector = handDetector()
-
     while True:
         success, img = cap.read()
         if not success:
