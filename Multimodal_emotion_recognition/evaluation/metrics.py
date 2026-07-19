acf72394318f82c6053356c1d408670f414661f7
trunnguyen <137796467+trunnguyen@users.noreply.github.com>
2026-07-19 12:23:33 +0700

    Update ignore rules and untrack local project folders

--- Multimodal_emotion_recognition/evaluation/metrics.py
+++
@@ -1,7 +0,0 @@
-from sklearn.metrics import accuracy_score, f1_score
-
-def evaluate(y_true, y_pred):
-    return {
-        "accuracy": accuracy_score(y_true, y_pred),
-        "f1": f1_score(y_true, y_pred, average="weighted")
-    }
