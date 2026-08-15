517409de9f98838dcc4e1b3ac7591f3c30501cc7
trunnguyen <137796467+trunnguyen@users.noreply.github.com>
2026-08-15 12:08:37 +0700

    Add SMOTE

--- autonomous_job_finder/src/analytics/training_pipeline.py
+++ autonomous_job_finder/src/analytics/training_pipeline.py
@@ -2,6 +2,8 @@ import os
 import sys
 import pickle
 import numpy as np
+from imblearn.over_sampling import SMOTE
+
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import StratifiedKFold,train_test_split
 from sklearn.metrics import f1_score, classification_report
@@ -44,6 +46,10 @@ def run_model_training():
     logger.info(f"Vectorizing {len(texts)} positions")
     X = recommender.embedding_model.encode(texts, show_progress_bar=False)
 
+    #Add SMOTE distribution
+    logger.info(f"Class distribution before SMOTE  - Pos: {num_positives}, Neg: {num_negatives}")
+    sm= SMOTE(random_state=42, k_neighbors=min(5,num_positives - 1))
+
     logger.info("Evaluating the model StratifiedKFold")
     skf = StratifiedKFold(n_splits=3, shuffle=True, random_state=42)
     cv_score=[]
@@ -52,6 +58,9 @@ def run_model_training():
     for fold, (train_index, test_index) in enumerate(skf.split(X, y),1):
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index], y[test_index]
+
+        #Add sample to SMOTE
+        X_train, y_train = sm.fit_resample(X_train, y_train)
 
         fold_clf= LogisticRegression(class_weight='balanced',random_state=42)
         fold_clf.fit(X_train, y_train)
@@ -71,8 +80,13 @@ def run_model_training():
     print("=" * 60)
 
     logger.info("Fitting model")
+    #
+    X_resampled, y_resampled = sm.fit_resample(X, y)
+    logger.info(f"After SMOTE - Pos: {np.sum(y_resampled==1)}, Neg: {np.sum(y_resampled==0)}")
     clf = LogisticRegression(class_weight='balanced')
-    clf.fit(X, y)
+    clf.fit(X_resampled, y_resampled)
+
+
 
     with open(model_pkl_path, 'wb') as f:
         pickle.dump(clf, f)
